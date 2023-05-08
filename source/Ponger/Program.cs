906b1709376df75f5d9bce9e7afbb64e836ba3c2
Hans Løken <hans@ebamse.net>
2023-05-08 15:47:33 +0200

    Added Contracts + Pinger and Ponger to solution. WIP

---
+++ source/Ponger/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Confluent.Kafka;
+using System.Net;
+using System.Text.Json.Nodes;
+using Contracts;
+using Newtonsoft.Json;
+
+
+var config = new ProducerConfig
+{
+    BootstrapServers = "host1:9092",
+};
+
+using (var producer = new ProducerBuilder<Null, string>(config).Build())
+{
+    var message = new Message<string, string> { Value=JsonSerializer. new Pong() };
+    var result = await producer.ProduceAsync("demo-topic", message);
+}
